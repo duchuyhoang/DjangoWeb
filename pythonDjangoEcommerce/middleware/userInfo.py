6de25a9173b584701401baf5ad97df0c3e8cfff2
Huy Hoang <huyhoang10032000@gmail.com>
2022-04-15 21:33:26 +0700

    hot fix sign up

--- pythonDjangoEcommerce/middleware/userInfo.py
+++ pythonDjangoEcommerce/middleware/userInfo.py
@@ -5,8 +5,10 @@ from pythonDjangoEcommerce.common.ultils import getUserInfo
 
 def userInfo(get_response):
     def middleware(request: HttpRequest):
-        if(request.user.id is not None):
-            request.userInfo = getUserInfo(request)[0] or None
+        userInfo = getUserInfo(request)
+        print(userInfo)
+        if(request.user.id is not None and len(userInfo) != 0):
+            request.userInfo = userInfo[0] or None
         else:
             request.userInfo = None
         response = get_response(request)
